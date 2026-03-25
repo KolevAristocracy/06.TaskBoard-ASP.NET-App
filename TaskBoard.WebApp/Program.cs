f41f9899ecf1d87e66fcf9f9da77cf8f10a17e6e
KolevAristocracy <kalink300@gmail.com>
2026-03-25 12:31:12 +0200

    change builder services for postgresql

--- TaskBoard.WebApp/Program.cs
+++ TaskBoard.WebApp/Program.cs
@@ -9,8 +9,8 @@ using TaskBoard.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString)); ;
+builder.Services.AddDbContext<MyDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services
     .AddDefaultIdentity<User>(options =>
